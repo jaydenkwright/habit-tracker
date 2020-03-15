33483c56e523ba28ae40d6e2e4746942f737fb27
jaydenkwright <jaydenkwright3@gmail.com>
2020-03-15 09:05:03 -0400

    History API can now query through specific days

--- api/completed.js
+++ api/completed.js
@@ -1,10 +1,26 @@
 const express = require('express')
 const router = express.Router()
 const Completed = require('../models/completions')
+const moment = require('moment')
+
+// router.get('/:id/:date', async (req, res) => {
+//     try{
+//         const completions = await Completed.find({"_id": req.params.id, "date": req.params.date })
+//             .then(completions => res.json(completions))
+//     }catch(err){
+//         res.json({message: error})
+//     }
+// })
 
 router.get('/:id/:date', async (req, res) => {
     try{
-        const completions = await Completed.find({"_id": req.params.id, "date": req.params.date })
+        const start = moment(new Date(req.params.date)).startOf("day")
+        const end = moment(new Date(req.params.date)).endOf("day")
+        //res.json(`start: ${start} end: ${end}`)
+        const completions = await Completed.find({"_id": req.params.id, "date": {
+            $gte: start,
+            $lt: end
+        } })
             .then(completions => res.json(completions))
     }catch(err){
         res.json({message: error})
--- client/src/App.js
+++ client/src/App.js
@@ -7,9 +7,12 @@ import Context from './Context/Context'
 import AddIcon from '@material-ui/icons/Add';
 import Button from '@material-ui/core/Button';
 import axios from 'axios'
+import moment from 'moment'
 
 function App() {
-
+  const start = moment(new Date('2020-03-06T01:56:44.202Z')).startOf("day")
+  const end = moment().endOf("day")
+  console.log(start)
   const [habitData, setHabitData] = useState([])
   useEffect(() => {
     axios.get('http://localhost:5000/api/habits')
